b31bdac06a061af6541330a77582420e8b41511e
Jan Skrzypczak <jan.skrz@gmail.com>
2014-09-05 00:24:38 +0200

    Added utility methods to easily distinguish between lifecycle and stat change events

--- app/src/main/java/com/rumoe/speedtouch/game/event/GameEvent.java
+++ app/src/main/java/com/rumoe/speedtouch/game/event/GameEvent.java
@@ -1,5 +1,7 @@
 package com.rumoe.speedtouch.game.event;
 
+import android.util.Log;
+
 import com.rumoe.speedtouch.game.gameboard.CellPosition;
 
 /**
@@ -29,6 +31,25 @@ public class GameEvent {
 
     public CellPosition getCausingCell() {
         return causingCell;
+    }
+
+    public boolean isLifecycleEvent() {
+        switch (type) {
+            case COUNTDOWN_START:
+            case GAME_START:
+            case GAME_OVER: return true;
+            case SCORE_CHANGE:
+            case LIFE_CHANGE: return false;
+            default:
+                // added this to make sure to not forget to update this method
+                // in case of adding more event types
+                Log.w("GameEvent", "Unknown EvenType as checking classification");
+        }
+        return false;
+    }
+
+    public boolean isStatEvent() {
+        return !isLifecycleEvent();
     }
 }
 
---
+++ app/src/main/java/com/rumoe/speedtouch/game/event/GameObserver.java
@@ -0,0 +1,9 @@
+package com.rumoe.speedtouch.game.event;
+
+/**
+ * Created by jan on 05.09.2014.
+ */
+public interface GameObserver {
+
+    void notify(GameEvent e);
+}
