8f0cc766de147d4990f8c5eb78e854b2470e3077
Jan Skrzypczak <jan.skrz@gmail.com>
2014-09-06 16:28:18 +0200

    Moved most funktionality from gameboard fragment to gameactivity because that makes way more sense

--- app/src/main/java/com/rumoe/speedtouch/game/GameActivity.java
+++ app/src/main/java/com/rumoe/speedtouch/game/GameActivity.java
@@ -1,22 +1,83 @@
 package com.rumoe.speedtouch.game;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.rumoe.speedtouch.R;
+import com.rumoe.speedtouch.game.event.GameEvent;
+import com.rumoe.speedtouch.game.event.GameEventManager;
+import com.rumoe.speedtouch.game.event.GameLifecycleEvent;
+import com.rumoe.speedtouch.game.event.GameObserver;
+import com.rumoe.speedtouch.game.strategy.textview.GameLifeUpdater;
+import com.rumoe.speedtouch.game.strategy.textview.GameScoreUpdater;
+import com.rumoe.speedtouch.game.strategy.textview.SurvivalLifeUpdater;
+import com.rumoe.speedtouch.game.strategy.textview.SurvivalScoreUpdater;
+import com.rumoe.speedtouch.menu.TempStart;
 
-public class GameActivity extends Activity {
+public class GameActivity extends Activity implements GameObserver {
+
+    private GameThread          gameThread;
+    private GameBoardFragment   gameBoard;
+    private GameScoreUpdater    scoreUpdater;
+    private GameLifeUpdater     lifeUpdater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         if (savedInstanceState == null) {
+            gameBoard = new GameBoardFragment();
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new GameBoardFragment())
+                    .add(R.id.container, gameBoard)
                     .commit();
         }
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
 
+        GameEventManager.getInstance().register(this);
+
+        //TODO for now scoreUpdater and lifeUpdater will be hardcoded.. change that at some point
+        scoreUpdater = new SurvivalScoreUpdater(this);
+        lifeUpdater = new SurvivalLifeUpdater(this);
+        gameThread = new GameThread(gameBoard.getCells());
+
+        gameBoard.subscribeToCells(scoreUpdater, lifeUpdater, gameThread);
+
+        //TODO temporary clusterfuck
+        new Thread() {
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {}
+
+                GameEventManager.getInstance()
+                        .notifyAll(new GameLifecycleEvent(GameEvent.EventType.GAME_START));
+            }
+        }.start();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        gameBoard.unsubscribeToCells(scoreUpdater, lifeUpdater, gameThread);
+        GameEventManager.getInstance().unregisterAll();
+        gameThread.gameOver();
+        transitionToMenu();
+    }
+
+    @Override
+    public void notifyOnGameEvent(GameEvent e) {
+        if (e.getType().equals(GameEvent.EventType.GAME_OVER)) {
+            transitionToMenu();
+        }
+    }
+
+    private void transitionToMenu() {
+        Intent intent = new Intent(this, TempStart.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/rumoe/speedtouch/game/GameBoardFragment.java
+++ app/src/main/java/com/rumoe/speedtouch/game/GameBoardFragment.java
@@ -1,7 +1,6 @@
 package com.rumoe.speedtouch.game;
 
 import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,26 +10,14 @@ import android.widget.TableRow;
 
 import com.rumoe.speedtouch.R;
 import com.rumoe.speedtouch.game.event.CellObserver;
-import com.rumoe.speedtouch.game.event.GameEvent;
-import com.rumoe.speedtouch.game.event.GameEventManager;
-import com.rumoe.speedtouch.game.event.GameLifecycleEvent;
-import com.rumoe.speedtouch.game.event.GameObserver;
 import com.rumoe.speedtouch.game.gameboard.Cell;
-import com.rumoe.speedtouch.game.strategy.textview.GameLifeUpdater;
-import com.rumoe.speedtouch.game.strategy.textview.GameScoreUpdater;
-import com.rumoe.speedtouch.game.strategy.textview.SurvivalLifeUpdater;
-import com.rumoe.speedtouch.game.strategy.textview.SurvivalScoreUpdater;
-import com.rumoe.speedtouch.menu.TempStart;
 
-public class GameBoardFragment extends Fragment implements GameObserver {
+public class GameBoardFragment extends Fragment {
 
     private static final int ROW_COUNT      = 5;
     private static final int COLUMN_COUNT   = 3;
 
-    private Cell[][] cells;
-    private GameThread thread;
-    private GameScoreUpdater scoreUpdater;
-    private GameLifeUpdater lifeUpdater;
+    private final Cell[][] cells;
 
     public GameBoardFragment() {
         cells = new Cell[ROW_COUNT][COLUMN_COUNT];
@@ -60,41 +47,7 @@ public class GameBoardFragment extends Fragment implements GameObserver {
         return rootView;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        GameEventManager.getInstance().register(this);
-
-        //TODO for now scoreUpdater and lifeUpdater will be hardcoded.. change that at some point
-        scoreUpdater = new SurvivalScoreUpdater(getActivity());
-        lifeUpdater = new SurvivalLifeUpdater(getActivity());
-        thread = new GameThread(cells);
-
-        subscribeToCells(scoreUpdater, lifeUpdater, thread);
-
-        //TODO temporary clusterfuck
-        new Thread() {
-            public void run() {
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {}
-
-                GameEventManager.getInstance()
-                    .notifyAll(new GameLifecycleEvent(GameEvent.EventType.GAME_START));
-            }
-        }.start();
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        unsubscribeToCells(scoreUpdater, lifeUpdater, thread);
-        GameEventManager.getInstance().unregisterAll();
-        thread.gameOver();
-    }
-
-    private void subscribeToCells(CellObserver... obs) {
+    public void subscribeToCells(CellObserver... obs) {
         for (int i = 0; i < ROW_COUNT; i++) {
             for (int j = 0; j < COLUMN_COUNT; j++) {
                 for (CellObserver o : obs) {
@@ -104,7 +57,7 @@ public class GameBoardFragment extends Fragment implements GameObserver {
         }
     }
 
-    private void unsubscribeToCells(CellObserver... obs) {
+    public void unsubscribeToCells(CellObserver... obs) {
         for (int i = 0; i < ROW_COUNT; i++) {
             for (int j = 0; j < COLUMN_COUNT; j++) {
                 for (CellObserver o : obs) {
@@ -114,15 +67,7 @@ public class GameBoardFragment extends Fragment implements GameObserver {
         }
     }
 
-    @Override
-    public void notifyOnGameEvent(GameEvent e) {
-        if (e.getType().equals(GameEvent.EventType.GAME_OVER)) {
-            transitionToMenu();
-        }
-    }
-
-    private void transitionToMenu() {
-        Intent intent = new Intent(this.getActivity(), TempStart.class);
-        startActivity(intent);
+    public Cell[][] getCells() {
+        return cells;
     }
 }
