1822aa80e95fa20bba215548496196385b40cb65
Jan Skrzypczak <jan.skrz@gmail.com>
2014-09-16 21:48:20 +0200

    Added highscore activity skeleton

---
+++ app/src/main/java/com/rumoe/speedtouch/menu/HighscoreActivity.java
@@ -0,0 +1,16 @@
+package com.rumoe.speedtouch.menu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.rumoe.speedtouch.R;
+
+
+public class HighscoreActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highscore);
+    }
+}
