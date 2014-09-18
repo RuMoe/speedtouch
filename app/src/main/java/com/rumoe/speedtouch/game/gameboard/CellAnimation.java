ee9233abde242fb5ab8787232070a1c2cc0ee755
Jan Skrzypczak <jan.skrz@gmail.com>
2014-09-18 17:08:59 +0200

    The way the grow/shrink animation is handled is completely stupid and error-prone. Removed everything what needs to be replaced

--- app/src/main/java/com/rumoe/speedtouch/game/gameboard/CellAnimation.java
+++ app/src/main/java/com/rumoe/speedtouch/game/gameboard/CellAnimation.java
@@ -13,7 +13,7 @@ import com.rumoe.speedtouch.game.strategy.cellradius.CellRadiusCalcStrategy;
 import com.rumoe.speedtouch.game.strategy.cellradius.ExponentialStrategy;
 import com.rumoe.speedtouch.game.strategy.cellradius.LinearStrategy;
 
-public class CellAnimation implements Runnable {
+public class CellAnimation{
 
     private Context context;
     private CellType cellType;
@@ -27,16 +27,11 @@ public class CellAnimation implements Runnable {
     private float cellXCenter;
     private float cellYCenter;
 
-    private Thread drawThread;
-    private CellRadiusCalc calculationThread;
-    private boolean animationRunning;
-
     public CellAnimation(SurfaceHolder surface, CellType cellType, Context context) {
         this.context = context;
         this.cellType = cellType;
 
         cellSurface = surface;
-        animationRunning = false;
 
         updatePaint();
     }
@@ -71,6 +66,48 @@ public class CellAnimation implements Runnable {
     }
 
     /**
+     * Check if given point lies in the currently displayed circle.
+     * @param xCoord x-Coordinate of the point.
+     * @param yCoord y-Coordinate of the point.
+     * @return true iff the point is on or in the circle, false otherwise
+     */
+    public boolean isTargetHit(float xCoord, float yCoord) {
+        float xDif = xCoord - cellXCenter;
+        float yDif = yCoord - cellYCenter;
+
+        return xDif*xDif + yDif*yDif <= currentCellRadius*currentCellRadius;
+    }
+
+    public boolean setDefaultGrowAnimation(int duration) {
+        return setAnimation(new LinearStrategy(), duration, currentCellRadius, maxCellRadius);
+    }
+
+    public boolean setDefaultShrinkAnimation(int duration) {
+        return setAnimation(new ExponentialStrategy(), duration, currentCellRadius, minCellRadius);
+    }
+
+    public boolean setDefaultBlinkAnimation(int duration) {
+        return setAnimation(new BlinkStategy(), duration, maxCellRadius, minCellRadius);
+    }
+
+    public boolean setAnimation(CellRadiusCalcStrategy strategy, int duration, float startSize, float targetSize) {
+
+        return true;
+    }
+
+    /**
+     * Clear the content of the cell and stop all animations.
+     * @return true
+     */
+    public boolean clearCell() {
+        currentCellRadius = 0;
+        clearBackground();
+        return true;
+    }
+
+    // TODO stopanimation isAnimationRunning waituntilanimationended
+
+    /**
      * Fills the cell with its background color.
      * @return true iif operation executed successfully, false otherwise
      */
@@ -89,212 +126,6 @@ public class CellAnimation implements Runnable {
             // we need this in case the surface is destroyed during drawing
             Log.e("CellAnimation", "Surface destroyed while trying to clear background");
             return false;
-        }
-    }
-
-    /**
-     * Check if given point lies in the currently displayed circle.
-     * @param xCoord x-Coordinate of the point.
-     * @param yCoord y-Coordinate of the point.
-     * @return true iff the point is on or in the circle, false otherwise
-     */
-    public boolean isTargetHit(float xCoord, float yCoord) {
-        float xDif = xCoord - cellXCenter;
-        float yDif = yCoord - cellYCenter;
-
-        return xDif*xDif + yDif*yDif <= currentCellRadius*currentCellRadius;
-    }
-
-    public boolean isAnimationRunning() {
-        return (drawThread != null && drawThread.isAlive());
-    }
-
-    public boolean setDefaultGrowAnimation(int duration) {
-        return setAnimation(new LinearStrategy(), duration, currentCellRadius, maxCellRadius);
-    }
-
-    public boolean setDefaultShrinkAnimation(int duration) {
-        return setAnimation(new ExponentialStrategy(), duration, currentCellRadius, minCellRadius);
-    }
-
-    public boolean setDefaultBlinkAnimation(int duration) {
-        return setAnimation(new BlinkStategy(), duration, maxCellRadius, minCellRadius);
-    }
-
-    public boolean setAnimation(CellRadiusCalcStrategy strategy, int duration, float startSize, float targetSize) {
-       if (isAnimationRunning()) return false;
-
-        calculationThread = new CellRadiusCalc(strategy, duration, startSize, targetSize);
-        calculationThread.start();
-
-        drawThread = new Thread(this);
-        drawThread.start();
-
-        return true;
-    }
-
-
-    /**
-     * Blocks the calling thread until the animation is ended.
-     * @return false iff block was interrupted, true otherwise.
-     */
-    public boolean waitUntilAnimationEnded() {
-        try {
-            drawThread.join();
-        } catch (InterruptedException e) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Clear the content of the cell and stop all animations.
-     * @return true
-     */
-    public boolean clearCell() {
-        stopAnimation();
-
-        currentCellRadius = 0;
-        clearBackground();
-        return true;
-    }
-
-    /**
-     * Stop all running threads.
-     */
-    public void stopAnimation() {
-        animationRunning = false;
-
-        if (calculationThread != null) {
-            calculationThread.abortCalculations();
-        }
-
-        if (drawThread != null) {
-            drawThread.interrupt();
-        }
-    }
-
-    @Override
-    /**
-     * As long as the thread for recalculation of the cell radius works, update
-     * the cell for the user in regular intervals.
-     */
-    public void run() {
-        int framesDrawn = 0;
-        try {
-            // the variable oneMore is necessary the make sure that no radius changes are made
-            // which are not drawn.
-            // Without it, the radius calc thread could finish and set animationRunning to false
-            // in between the canvas draw and the next iteration --> the last changes are lost which
-            // would lead to an inconsistent cell-size.
-            boolean oneMore = true;
-            while (oneMore) {
-                oneMore = animationRunning;
-
-                if (cellSurface.getSurface().isValid()) {
-                    Canvas canvas = cellSurface.lockCanvas();
-
-                    // over-paint everything from previous frame
-                    canvas.drawColor(context.getResources().getColor(R.color.game_board_background));
-                    canvas.drawCircle(cellXCenter, cellYCenter, currentCellRadius, cellPaint);
-
-                    cellSurface.unlockCanvasAndPost(canvas);
-                } else {
-                    Log.e("CellAnimation", "Cell surface invalid while attempting to draw");
-                }
-
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    Log.w("CellAnimation", "Sleep in cell draw thread interrupted");
-                    return;
-                }
-                framesDrawn++;
-            }
-        } catch (NullPointerException e) {
-            // we need this in case the surface is destroyed during drawing
-            Log.e("CellAnimation", "Surface destroyed while trying to draw the animation ");
-            // end the thread because we cannot do anything anymore
-            stopAnimation();
-            return;
-        }
-
-        Log.d("CellAnimation", "" + framesDrawn + " frames during animation drawn.");
-    }
-
-    /**
-     * This runnable' task is it to change the cell radius from zero - max or vise versa
-     * in a given time with given number of steps.
-     */
-    private class CellRadiusCalc extends Thread {
-
-        private CellRadiusCalcStrategy radiusCalcStrategy;
-
-        private boolean abortCalc;
-
-        private float startRadius;
-        private float targetRadius;
-        private int numberOfSteps;
-        private int duration;
-
-        CellRadiusCalc (CellRadiusCalcStrategy strategy, int duration, float startRadius, float targetRadius) {
-            this(strategy, duration, duration / 2, startRadius, targetRadius);
-        }
-
-        CellRadiusCalc (CellRadiusCalcStrategy strategy, int duration, int numberOfSteps,
-                        float startRadius, float targetRadius) {
-            this.radiusCalcStrategy = strategy;
-            this.duration = duration;
-            this.numberOfSteps = numberOfSteps;
-            this.targetRadius = targetRadius;
-            this.startRadius = startRadius;
-
-            abortCalc = false;
-            animationRunning = true;
-        }
-
-        void abortCalculations() {
-            abortCalc = true;
-            interrupt();
-        }
-
-        @Override
-        public void run() {
-            int currentStep = 1;
-
-            long start = System.currentTimeMillis();
-
-            while (currentStep < numberOfSteps) {
-                int remainingSteps = numberOfSteps - currentStep + 1;
-
-                if (abortCalc) return;
-
-                currentCellRadius = radiusCalcStrategy.calculateRadius(startRadius, targetRadius,
-                        currentCellRadius, currentStep, numberOfSteps);
-
-                if (abortCalc) return;
-                try {
-                    long remainingTime = Math.abs(System.currentTimeMillis() - start - duration);
-                    float time = ((float) remainingTime) / remainingSteps;
-                    Thread.sleep((int) time, ((int) (time * 1000000)) % 1000000);
-                } catch (InterruptedException e) {
-                    Log.w("CellAnimation", "Sleep in cell radius calculation thread interrupted");
-                    return;
-                }
-                currentStep++;
-            }
-
-            long end = System.currentTimeMillis();
-
-            if (end - start - duration > 10) {
-                Log.w("CellAnimation", "Animation took longer than expected: " + (end-start) +
-                        "ms. Expected " + duration);
-            } else {
-                Log.d("CellAnimation", "Animation duration: " + (end-start) +
-                        "ms. Expected " + duration);
-            }
-
-            animationRunning = false;
         }
     }
 }
