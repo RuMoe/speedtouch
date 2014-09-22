fc1a903b7ddcd117f9982bd5f8d18748d71a0e63
Jan Skrzypczak <jan.skrz@gmail.com>
2014-09-22 23:52:33 +0200

    While we are refactoring could also add some comments...

--- app/src/main/java/com/rumoe/speedtouch/game/ui/GameBoardFragment.java
+++ app/src/main/java/com/rumoe/speedtouch/game/ui/GameBoardFragment.java
@@ -54,23 +54,56 @@ public class GameBoardFragment extends Fragment {
         }
     }
 
+    /**
+     * Can be used to determine if a cell is currently active.
+     * @param pos The cell to be checked.
+     * @return true iff the cell is active, false otherwise.
+     */
     public boolean isCellActive(CellPosition pos) {
         // TODO
         return true;
     }
 
+    /**
+     * Activated the cell on the specified position. In contrast to the lifecycle method, the cell
+     * we be visible indefinitely.
+     * When calling this method successfully the cell will emit an CellEvent.ACTIVATED event.
+     * @param pos The position of the cell which will be activated.
+     * @param type Type the cell will have.
+     * @return true iff animation could be applied successfully, false otherwise.
+     */
     public boolean activateCell(CellPosition pos, CellType type) {
         if (isCellActive(pos)) return false;
         // TODO
         return true;
     }
 
+    /**
+     * Starts the lifecycle of the cell on a specified position. A lifecycle contains three stages:
+     * grow, constant size and shrink.
+     * Calling this method will use the default timing of the lifecycle.
+     * When calling this method successfully the cell will emit an CellEvent.ACTIVATED event.
+     * @param pos The position of the cell which will be activated.
+     * @param type Type the cell will have.
+     * @return true iff animation could be applied successfully, false otherwise.
+     */
     public boolean activateCellLifeCycle(CellPosition pos, CellType type) {
         if (isCellActive(pos)) return false;
         // TODO
         return true;
     }
 
+    /**
+     * Starts the lifecycle of the cell on a specified position. A lifecycle contains three stages:
+     * grow, constant size and shrink.
+     * When calling this method successfully the cell will emit an CellEvent.ACTIVATED event.
+     * @param pos The position of the cell which will be activated.
+     * @param type Type the cell will have.
+     * @param growTime Time in ms for the grow phase of the lifecycle.
+     * @param stayTime Time in ms for the constant size phase of the lifecycle.
+     * @param shrinkTime Time in ms for the shrink phase of the lifecycle.
+     * @return true iff animation could be applied successfully, false otherwise.
+     */
     public boolean activateCellLifeCycle(CellPosition pos, CellType type,
                                          int growTime, int stayTime, int shrinkTime) {
         if (isCellActive(pos)) return false;
@@ -78,21 +111,40 @@ public class GameBoardFragment extends Fragment {
         return true;
     }
 
+    /**
+     * Starts the default blink animation of the cell on the specified position.
+     * @param pos The position of the cell which shall blink.
+     * @return true iff animation could be applied successfully, false otherwise.
+     */
     public boolean blinkCell(CellPosition pos) {
         if (isCellActive(pos)) return false;
         // TODO
         return true;
     }
 
+    /**
+     * Clears the cell at the specified position. That means its state is set to deactivated and
+     * all animations are stopped.
+     * @param pos The position of the cell we want to clear.
+     * @return true iff clear was successful, false otherwise.
+     */
     public boolean clearCell(CellPosition pos) {
         // TODO
         return true;
     }
 
+    /**
+     * Gets the total amount of rows of the game board.
+     * @return rows of the game board.
+     */
     public int getRowCount() {
         return ROW_COUNT;
     }
 
+    /**
+     * Gets the total amount of columns of the game board.
+     * @return columns of the game board.
+     */
     public int getColumnCount() {
         return COLUMN_COUNT;
     }
