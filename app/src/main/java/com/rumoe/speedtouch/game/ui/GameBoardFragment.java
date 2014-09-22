6a882526e1ce1fc2c6f0686176714a15b28afc9f
Jan Skrzypczak <jan.skrz@gmail.com>
2014-09-22 23:23:35 +0200

    Turns out multiple SurfaceViews is a fucking stupid idea *sight* Refactor the game board code again... So instead of a table now only one SurfaceView!

--- app/src/main/java/com/rumoe/speedtouch/game/ui/GameBoardFragment.java
+++ app/src/main/java/com/rumoe/speedtouch/game/ui/GameBoardFragment.java
@@ -3,10 +3,9 @@ package com.rumoe.speedtouch.game.ui;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 
 import com.rumoe.speedtouch.R;
 import com.rumoe.speedtouch.game.event.CellObserver;
@@ -19,6 +18,7 @@ public class GameBoardFragment extends Fragment {
     private static final int COLUMN_COUNT   = 3;
 
     private final Cell[][] cells;
+    private SurfaceView gameBoard;
 
     public GameBoardFragment() {
         cells = new Cell[ROW_COUNT][COLUMN_COUNT];
@@ -27,25 +27,8 @@ public class GameBoardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView           = inflater.inflate(R.layout.fragment_game, container, false);
-        final TableLayout gameBoard   = (TableLayout) rootView.findViewById(R.id.gameBoard);
-
-        for (int i = 0; i < ROW_COUNT; i++) {
-            for (int j = 0; j < COLUMN_COUNT; j++) {
-                cells[i][j] = new Cell(getActivity(), j, i);
-            }
-        }
-
-        final float cellHeight = 1.0f / ROW_COUNT;
-        final float cellWidth = 1.0f / COLUMN_COUNT;
-
-        for (int i = 0; i < ROW_COUNT; i++) {
-            final TableRow tr = new TableRow(gameBoard.getContext());
-            for (int j = 0; j < COLUMN_COUNT; j++) {
-                tr.addView(cells[i][j], new TableRow.LayoutParams(0, TableRow.LayoutParams.MATCH_PARENT, cellWidth));
-            }
-            gameBoard.addView(tr, new TableLayout.LayoutParams(0, 0, cellHeight));
-        }
+        View rootView   = inflater.inflate(R.layout.fragment_game, container, false);
+        gameBoard       = (SurfaceView) rootView.findViewById(R.id.gameBoard);
 
         return rootView;
     }
