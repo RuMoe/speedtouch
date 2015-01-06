5e8a33466f2d628d40d34cc4ab8e3941ee8c0828
Jan Skrzypczak <jan.skrz@gmail.com>
2015-01-07 00:03:07 +0100

    replace shadowlayer with blur effect

--- app/src/main/java/com/rumoe/speedtouch/game/ui/GameBoardFragment.java
+++ app/src/main/java/com/rumoe/speedtouch/game/ui/GameBoardFragment.java
@@ -1,6 +1,7 @@
 package com.rumoe.speedtouch.game.ui;
 
 import android.app.Fragment;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.os.Bundle;
@@ -355,8 +356,7 @@ public class GameBoardFragment extends Fragment implements SurfaceHolder.Callbac
         public Paint getPaint(CellType type) {
             Paint paint = new Paint();
             paint.setColor(CellType.getCellColor(type, GameBoardFragment.this.getActivity()));
-            paint.setShadowLayer(15.0f, 0.0f, 0.0f,
-                    CellType.getShadowColor(type, GameBoardFragment.this.getActivity()));
+            paint.setMaskFilter(new BlurMaskFilter(15, BlurMaskFilter.Blur.SOLID));
             return paint;
         }
     }
